82662bfececb5a300718726d6cc8c6015235e848
jacob <jacobkurian30@gmail.com>
2018-12-17 00:28:45 -0500

    initila commit

---
+++ Assets/PlazzleBlockImplement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlazzleBlockImplement : MonoBehaviour
+{
+    
+    public GameObject buttonPrefab;
+    public Transform buttonContainer;
+
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+       // GameObject gameObject = (GameObject)Resources.Load("button", typeof(GameObject)); 
+        GameObject gameObject = Instantiate(buttonPrefab) as GameObject;
+        gameObject.transform.SetParent(buttonContainer);
+        gameObject.GetComponentInChildren<Text>().text =  "Hellow wordl";
+    }
+
+    public void Setup()
+    {
+        
+    }
+
+    public void HandleClick()
+    {
+        
+    }
+}
