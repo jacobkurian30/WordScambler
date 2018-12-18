33e937a0eed33a757948fcb596930834b6ee08a0
jacob <jacobkurian30@gmail.com>
2018-12-17 23:51:24 -0500

    added basic UI elements
    Added Buttons, make it move from top to button

--- Assets/PlazzleBlockImplement.cs
+++ Assets/PlazzleBlockImplement.cs
@@ -6,29 +6,59 @@ using UnityEngine.UI;
 
 public class PlazzleBlockImplement : MonoBehaviour
 {
-    
+
     public GameObject buttonPrefab;
     public Transform buttonContainer;
+    private int rowWidth = 0;
 
 
-    
+    private GameObject objectOne;
+   //TODO create object after the other one land on the 0.
 
     // Use this for initialization
     void Start()
     {
-       // GameObject gameObject = (GameObject)Resources.Load("button", typeof(GameObject)); 
+        CanvasElements canvasElements = SetCanvasElement();
+
+        objectOne = createAlphabet("B", canvasElements);
+        rowWidth += Constants.SQUARE_SIDE;
+        createAlphabet("C", canvasElements);
+        rowWidth += Constants.SQUARE_SIDE;
+        createAlphabet("D", canvasElements);
+    }
+
+    private static CanvasElements SetCanvasElement()
+    {
+        CanvasElements canvasElements = new CanvasElements
+        {
+            Screen_height = Screen.height,
+            Screen_width = Screen.width
+        };
+        return canvasElements;
+    }
+
+    public GameObject createAlphabet(string letter, CanvasElements canvasElements)
+    {
         GameObject gameObject = Instantiate(buttonPrefab) as GameObject;
         gameObject.transform.SetParent(buttonContainer);
-        gameObject.GetComponentInChildren<Text>().text =  "Hellow wordl";
+        gameObject.transform.position = new Vector3(rowWidth + Constants.SQUARE_SIDE / 2, canvasElements.Screen_height, 0);
+        gameObject.GetComponentInChildren<Text>().text = letter;
+        return gameObject;
+    }
+
+
+    void Update()
+    {
+        objectOne.transform.position = new Vector3(objectOne.transform.position.x, objectOne.transform.position.y - 1, 0);
     }
 
     public void Setup()
     {
-        
+
     }
 
     public void HandleClick()
     {
-        
+
     }
 }
---
+++ Assets/Utility/CanvasElements.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasElements 
+{
+    private int row_width;
+    private int screen_width;
+    private int screen_height;
+
+    public int Row_width { get => row_width; set => row_width = value; }
+    public int Screen_width { get => screen_width; set => screen_width = value; }
+    public int Screen_height { get => screen_height; set => screen_height = value; }
+}
